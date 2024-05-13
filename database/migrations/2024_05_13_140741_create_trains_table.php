a66bb35cc8a516d50d3acd6c76f89a0ce283c128
enrico-amerio <amerio.enrico95@gmail.com>
2024-05-13 16:21:15 +0200

    first commit

---
+++ database/migrations/2024_05_13_140741_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug', 50);
+            $table->string('company', 50);
+            $table->string('firstStation', 50);
+            $table->string('lastStation', 50);
+            $table->time('arrivingTime');
+            $table->string('trainCode',20);
+            $table->tinyInteger('vagonNumber');
+            $table->boolean('is_onTime');
+            $table->boolean('is_cancelled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
